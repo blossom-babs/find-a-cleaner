10b7823276c6b853ecdd57b1d5a3e906d5e3625e
blossom-babs <saphashb@gmail.com>
2021-03-23 13:31:09 +0100

    build responsive header section

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import Clean from "./clean.png"
+
+export {Clean} 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,87 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Link} from "react-router-dom"
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Clean } from "../assets";
 
 const Header = () => {
   return (
     <Wrapper>
-      <div>
-        <img src="" alt="cleaning-service"/>
-        <div>
-          <p>Don't have the time to clean? 
-            We are here to match you with the 
-            perfect cleaning service.</p>
-            <button><Link to="/signup">Find a cleaner</Link></button>
+      <div className="container">
+        <div className="header">
+          <h1 className="header__h1">Need a hand with house chores?</h1>
+          <p className="header__p">
+            We are here to match you with the perfect service for all your
+            household chores.{" "}
+          </p>
+          <Link className="header__link" to="/signup">
+            Find a cleaner
+          </Link>
         </div>
+        <img className="header__img" src={Clean} alt="cleaning-service" />
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.header`
+position:relative;
+  .container {
+    font-family: var(--ff-body);
+    display: flex;
+    flex-direction: column;
+  }
 
-`
+  .header {
+    width: 90%;
+    margin: 0 auto;
+  }
 
-export default Header
+  .header__h1 {
+    font-family: var(--ff-heading);
+    color: var(--clr-base-two);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .header__p {
+    padding: 1em 0;
+    width: 40ch;
+  }
+
+  .header__link {
+    background: var(--clr-base-one);
+    width: max-content;
+    padding: 0.5em;
+    text-decoration: none;
+    color: var(--clr-base-light);
+  }
+  .header__link:hover {
+    transition: var(--transition-one);
+    background: var(--clr-base-two);
+  }
+
+  /* larger screens */
+  @media screen and (min-width: 768px) {
+
+    .header__h1 {
+      font-size:3rem;
+      margin-top:1em;
+      width:20ch;
+    }
+
+    .header__p{
+      font-size:2rem;
+      width:50ch;
+    }
+
+    .header__link {
+      font-size:2rem;
+   
+  }
+  .header__img{
+/* margin-top:-30em; */
+margin-top:-25%;
+  }
+  }
+`;
+
+export default Header;
